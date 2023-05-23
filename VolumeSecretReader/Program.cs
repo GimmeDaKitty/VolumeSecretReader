6f7f89920371d1532b85e3b7ec756b2e147eebe9
beatriz.del_saz <beatriz.del_saz@sita.aero>
2023-05-23 21:23:30 +0200

    Initial commit

--- VolumeSecretReader/Program.cs
+++ VolumeSecretReader/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+// load the configuration file.
+// The Microsoft.Extensions.Configuration.KeyPerFile library stores the configuration as follows:
+//// the filename is the key
+////  the file content is the value.
+
+// To truly make the 'reloadOnChange' work you need to set env variable DOTNET_USE_POLLING_FILE_WATCHER to true
+// in the docker/service deployment
+var configurationRoot = new ConfigurationBuilder()
+    .AddKeyPerFile(directoryPath: "/mnt/secrets-store/", optional: true, reloadOnChange:true)
+    .Build();
+
+while (true)
+{
+    var secretValue = configurationRoot["mySecret"] ?? "Secret not found";
+    Console.WriteLine(secretValue);
+    await Task.Delay(TimeSpan.FromSeconds(30));
+}
